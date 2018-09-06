9955055a98a14dd451c7e791ba89be22705901ba
Blazer <sstubbs012@gmail.com>
2018-09-05 23:03:00 -0400

    committing javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+// $(document).ready(function() {
+
+// function initialScreen() {
+// var startScreen = "<h1 class='text-center'>Nationals Trivia Game!</h1><p><button class='start-button'></button></p>";
+// $("#container").html(startScreen);
+// }
+// initialScreen();
+    
+//     $("#start").on("click", function() {
+//         countdown();
+//     });
+    
+    
+    
+    
+    
+    
+//     var timeLeft = 30;
+//     var timerId = setInterval(countdown, 1000);
+//     var elem = document.getElementById("time");
+    
+//     function countdown() {
+
+//       if (timeLeft === 0) {
+//         clearTimeout(timerId);
+//         Loser();
+//       } else {
+//         elem.innerHTML = "Time Remaining: " + timeLeft;
+//         timeLeft--;
+//       }
+//       console.log(timeLeft);
+//     };
+
+//     var correct = 0;
+//     var incorrect = 0;
+//     var unanswered = 0;
+
+//     $("#done").on("click", function () {
+//         $("#container").empty();
+
+
+//     });
+
+
+// });
+
+
+
